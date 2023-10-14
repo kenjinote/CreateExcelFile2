c9ee4fc6a5685e784367fe8ba678a1665d7dfde4
kenjinote <2605401+kenjinote@users.noreply.github.com>
2023-10-14 17:55:47 +0900

    プロジェクト ファイルを追加します。

---
+++ enc_temp_folder/89677ad92224d733d9388df0e498f94d/Source.cpp
@@ -0,0 +1,100 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib, "xlsxwriter.lib")
+#pragma comment(lib, "zlibstatic.lib")
+
+#include <windows.h>
+#include <xlsxwriter.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+void CreateExcelFile()
+{
+	// Excelファイルの新規作成
+	lxw_workbook* workbook = workbook_new("test.xlsx");
+
+	// シートの追加
+	lxw_worksheet* worksheet = workbook_add_worksheet(workbook, nullptr);
+
+	// テキストの入力
+	// 2番目の引数は行のインデックス、3番目の引数は列のインデックス
+	// いずれもゼロ開始(ゼロゼロがA1)
+	worksheet_write_string(worksheet, 0, 0, u8"表示するテキスト", nullptr);
+
+	// 数値の入力
+	worksheet_write_number(worksheet, 1, 0, 123.45, nullptr);
+	worksheet_write_number(worksheet, 2, 0, 654.32, nullptr);
+
+	// 式の入力
+	worksheet_write_formula(worksheet, 3, 0, "=SUM(A2:A3)", nullptr);
+
+	// 終了
+	workbook_close(workbook);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hButton;
+	static HWND hEdit;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("変換"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hButton, 10, 10, 256, 32, TRUE);
+		MoveWindow(hEdit, 10, 50, LOWORD(lParam) - 20, HIWORD(lParam) - 60, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			CreateExcelFile();
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPWSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("Window"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
---
+++ include/xlsxwriter.h
@@ -0,0 +1,25 @@
+/*
+ * libxlsxwriter
+ *
+ * Copyright 2014-2022, John McNamara, jmcnamara@cpan.org. See LICENSE.txt.
+ */
+
+/**
+ * @file xlsxwriter.h
+ *
+ * xlsxwriter - A library for creating Excel XLSX files.
+ *
+ */
+#ifndef __LXW_XLSXWRITER_H__
+#define __LXW_XLSXWRITER_H__
+
+#include "xlsxwriter/workbook.h"
+#include "xlsxwriter/worksheet.h"
+#include "xlsxwriter/format.h"
+#include "xlsxwriter/utility.h"
+
+#define LXW_VERSION "1.1.5"
+#define LXW_VERSION_ID 115
+#define LXW_SOVERSION "5"
+
+#endif /* __LXW_XLSXWRITER_H__ */
